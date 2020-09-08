50113240010d47e6debe22bca70b4d502d171032
Vinay Prasad <prasad.vinny@gmail.com>
2020-09-08 10:50:54 -0700

    Updates

---
+++ precache-manifest.ad85304f2288434a9fd63ae7962ff22a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7902ba70d95a8795e01a6b45838e26e7",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "6aebcf763e7975d04316",
+    "url": "/portfolio-site/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "1ca4828a90dd60a65d7e",
+    "url": "/portfolio-site/static/css/main.a42e979a.chunk.css"
+  },
+  {
+    "revision": "6aebcf763e7975d04316",
+    "url": "/portfolio-site/static/js/2.827cec5a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-site/static/js/2.827cec5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ca4828a90dd60a65d7e",
+    "url": "/portfolio-site/static/js/main.e705b164.chunk.js"
+  },
+  {
+    "revision": "a2a773adb23ff47de08a",
+    "url": "/portfolio-site/static/js/runtime-main.2d95e7a6.js"
+  },
+  {
+    "revision": "ccd89e8c14304e7fa8016863d120bcc6",
+    "url": "/portfolio-site/static/media/about-pic.ccd89e8c.png"
+  },
+  {
+    "revision": "04600a918939cfe3a635999550913b89",
+    "url": "/portfolio-site/static/media/back.04600a91.png"
+  },
+  {
+    "revision": "af4f55919503b0cf97c77631b8bb520a",
+    "url": "/portfolio-site/static/media/home-pic.af4f5591.png"
+  }
+]);
